956525d3b446c7f728532dd1fc970635c294ea97
Parthvi Vala <pvala@redhat.com>
2022-05-02 12:43:18 +0530

    Onboarded with odo

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const http = require('http');
 Prometheus.collectDefaultMetrics();
 
 const requestHistogram = new Prometheus.Histogram({
-    name: 'http_request_duration_seconds',
-    help: 'Duration of HTTP requests in seconds',
-    labelNames: ['code', 'handler', 'method'],
-    buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5]
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['code', 'handler', 'method'],
+  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5]
 })
 
 const requestTimer = (req, res, next) => {
@@ -33,8 +33,8 @@ const PRODUCTION = app.get('env') === 'production';
 
 // Administrative routes are not timed or logged, but for non-admin routes, pino
 // overhead is included in timing.
-app.get('/ready', (req, res) => res.status(200).json({status:"ok"}));
-app.get('/live', (req, res) => res.status(200).json({status:"ok"}));
+app.get('/ready', (req, res) => res.status(200).json({ status: "ok" }));
+app.get('/live', (req, res) => res.status(200).json({ status: "ok" }));
 app.get('/metrics', (req, res, next) => {
   res.set('Content-Type', Prometheus.register.contentType)
   res.end(Prometheus.register.metrics())
@@ -47,13 +47,13 @@ app.use(requestTimer);
 const pino = require('pino')({
   level: PRODUCTION ? 'info' : 'debug',
 });
-app.use(require('pino-http')({logger: pino}));
+app.use(require('pino-http')({ logger: pino }));
 
-app.get('/', (req, res) => {	
+app.get('/', (req, res) => {
   // Use req.log (a `pino` instance) to log JSON:	
-  req.log.info({message: 'Hello from Node.js Starter Application!'});		
-  res.send('Hello from Node.js Starter Application!');	
-});	
+  req.log.info({ message: 'Hello from Node.js Starter Application!' });
+  res.send('Hello from Node.js Starter Application Part 2!');
+});
 
 app.get('*', (req, res) => {
   res.status(404).send("Not Found");
